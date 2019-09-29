7e8c4eed84019a78e1a65808bb68fa0c742291eb
mikel <mikelpmc@gmail.com>
2019-09-29 17:35:58 +0200

    react intersection observer component

---
+++ README.md
@@ -0,0 +1,31 @@
+## React intersection Observer
+
+React component that uses the Intersection observer API.
+
+#### Getting started
+
+```shell
+npm install react-observer-component
+```
+
+#### Usage
+
+```jsx
+import React from 'react';
+import Observer from 'react-observer-component';
+
+const MyComponent = () => {
+    return (
+        <Observer>
+            {({ isInView, ref }) => {
+                <div ref={ref}>
+                    <h1>Intersection observer</h1>
+                    {isInView && <p>Only shows when is in viewport<p>}
+                </div>
+            }}
+        </Observer>
+    )
+};
+```
+
+##### Author: Mikel Parra <mikelpmc@gmail.com> | <http://github.com/mikelpmc>
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'observer.js',
+    path: path.resolve(__dirname, 'build/'),
+    publicPath: '/'
+  },
+  devtool: '#cheap-module-inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /(\.js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    stats: { colors: true },
+    inline: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './public/index.html' }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false
+    })
+  ]
+};
