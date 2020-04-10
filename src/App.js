2a4f77af95bcc73723987cddf360b14895552988
mikel <mikelpmc@gmail.com>
2020-04-10 16:26:28 +0200

    add example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import useObserver from './lib/';
 
 function App() {
-    return <div>Hellou Raect</div>;
+    const [isInView, ref] = useObserver();
+
+    return (
+        <div>
+            <p>
+                Open up the console network tab and scroll down, you will se how
+                the image would be loaded only when its visible on the viewport
+            </p>
+            <span role="img" aria-label="icon">
+                🤘
+            </span>
+
+            <div style={{ height: '2000px' }} />
+
+            <div ref={ref}>
+                {isInView && (
+                    <img src="http://placekitten.com/g/300/300" alt="kitten" />
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
