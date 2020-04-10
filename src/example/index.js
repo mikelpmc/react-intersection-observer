ecfdb6fc72b373307a705899c32d6f090716eb68
mikel <mikelpmc@gmail.com>
2020-04-10 15:44:27 +0200

    initial react-scripts project setup and cleanup

--- src/example/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Example = () => {
-  return <p>Hola</p>;
-};
-
-export default Example;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Example from './example';
+import App from './App';
 
-ReactDOM.render(<Example />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
